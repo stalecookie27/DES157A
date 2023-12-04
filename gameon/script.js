ba2c73908a93ca4b8d95880fa497dc2246ab090e
michellem <mimendoza@ucdavis.edu>
2023-12-04 03:47:21 -0800

    pushly

--- gameon/script.js
+++ gameon/script.js
@@ -1,90 +1,22 @@
-(function () {
-    'use strict';
-
-    console.log('reading js');
-
-    // buttons
-
-
-    const playGame = document.querySelector('#playgame');
+(function(){
+    'use strict'
+    console.log('reading JS');
+    
+    const startGame = document.querySelector('#startgame');
     const gameControl = document.querySelector('#gamecontrol');
-    const title = document.querySelector('#title');
     const game = document.querySelector('#game');
     const score = document.querySelector('#score');
     const actionArea = document.querySelector('#actions');
 
-    // sounds
-    const rollSound = new Audio('sounds/roll.m4a');
-    const passSound = new Audio('sounds/pass.m4a');
+    //overlay
+    const contButton = document.querySelector('#continue');
 
-    // if user has not played game before, instructions show
-    yes.addEventListener('click', function() {
-        document.querySelector('#overlay').className = "showing";
-        document.querySelector('#begin').remove();
-    });
-
-    // if user has played game before, game will automatically start
-    no.addEventListener('click', function() {
-        document.querySelector('#overlay').className = "hidden";
-        document.querySelector('#begin').remove();
-
-        // randomly set game index here
-        gameData.index = Math.round(Math.random());
-        console.log(gameData.index);
-    
-        // adding the buttons for the game control and the title
-        gameControl.innerHTML = '<img id="help" src="images/help.svg" alt="help icon">';
-        gameControl.innerHTML += '<button id="quit">Wanna Quit?</button>';
-        title.innerHTML = '<h1>Game of Pig</h1>';
-        title.style.marginTop = "0";
-    
-        // if user quits, browser refreshes
-        document.getElementById('quit').addEventListener('click', function() {
-            location.reload();
-        });
-    
-        // if user clicks help icon, instructions are displayed
-        document.getElementById('help').addEventListener('click', function() {
-            document.querySelector('#playgame').innerHTML = "Continue Game";
-            document.querySelector('#overlay').className = "showing";
-        });
-    
-        console.log("set up the turn!");
-    
-        setUpTurn();
-    });
-
-    // if escape is clicked, instructions are hidden and game starts
-    document.addEventListener('keydown', function(event) {
-        if(event.key === "Escape") {
-            document.getElementById('overlay').className = "hidden";
-            document.querySelector('#begin').remove();
-
-            // randomly set game index here
-            gameData.index = Math.round(Math.random());
-            console.log(gameData.index);
-        
-            gameControl.innerHTML = '<img id="help" src="images/help.svg" alt="help icon">';
-            gameControl.innerHTML += '<button id="quit">Wanna Quit?</button>';
-            title.innerHTML = '<h1>Game of Pig</h1>';
-        
-            document.getElementById('quit').addEventListener('click', function() {
-                location.reload();
-            });
-        
-            document.getElementById('help').addEventListener('click', function() {
-                document.querySelector('#playgame').innerHTML = "Continue Game";
-                document.querySelector('#overlay').className = "showing";
-            });
-        
-            console.log("set up the turn!");
-        
-            setUpTurn();
-        }
-    });
+    //sounds
+    const gulpSound = new Audio('music/gulping.mp3');
+    const pourSound = new Audio('music/pouring.mp3');
 
     const gameData = {
-        dice: ['images/1die.svg', 'images/2die.svg', 'images/3die.svg', 'images/4die.svg', 'images/5die.svg', 'images/6die.svg'],
+        dice: ['images/1die.jpg', 'images/2die.jpg', 'images/3die.jpg', 'images/4die.jpg', 'images/5die.jpg', 'images/6die.jpg'],
         players: ['player 1', 'player 2'],
         score: [0, 0],
         roll1: 0,
@@ -94,7 +26,8 @@
         gameEnd: 29
     };
 
-    /* //This gets the current player: 
+
+   /*  //This gets the current player: 
     gameData.players[gameData.index]
 
     //This gets the first die and the second die: 
@@ -115,127 +48,99 @@
     //This gets the winning threshold
     gameData.rollSum */
 
-    // game starts
-    playGame.addEventListener('click', function() {
-        document.querySelector('#overlay').className = "hidden";
+    contButton.addEventListener('click', function(event){
+        event.preventDefault();
+        document.getElementById('main').className = 'showing';
+        document.getElementById('overlay').className = 'hidden';
+    });
 
-        // randomly set game index here
+    document.querySelector('#gamerules').addEventListener('click', function(){
+        document.getElementById('overlay').className = 'showing';
+        startGame.remove();
+        document.querySelector('article').innerHTML = `<h3>INSTRUCTION</3> <button id="close">close</button>`
+        document.querySelector('#close').addEventListener('click',function(){
+            document.getElementById('overlay').className = 'hidden';
+        });
+    });
+        
+    startGame.addEventListener('click', function(){
+        pourSound.play();
+        // random set of game index
         gameData.index = Math.round(Math.random());
         console.log(gameData.index);
 
-        gameControl.innerHTML = '<img id="help" src="images/help.svg" alt="help icon">';
-        gameControl.innerHTML += '<button id="quit">Wanna Quit?</button>';
-        title.innerHTML = '<h1>Game of Pig</h1>';
-
-        document.getElementById('quit').addEventListener('click', function() {
+        gameControl.innerHTML = '<h2>THE GAME HAS STARTED</h2>';
+        gameControl.innerHTML += '<button id="quit">QUIT THE GAME</button>';
+        document.getElementById('quit').addEventListener('click', function(){
             location.reload();
         });
 
-        document.getElementById('help').addEventListener('click', function() {
-            document.querySelector('#playgame').innerHTML = "Continue Game";
-            document.querySelector('#overlay').className = "showing";
-            document
-        });
-
-        console.log("set up the turn!");
-
         setUpTurn();
     });
-    
-    // game tells user whose turn it is to roll
-    function setUpTurn() {
-        game.innerHTML = `<p>Roll the dice for the ${gameData.players[gameData.index]}</p>`;
+
+    function setUpTurn(){
+        startGame.remove();
+        game.innerHTML = `<p>Roll the dice for ${gameData.players[gameData.index]}</p>`;
         actionArea.innerHTML = '<button id="roll">Roll the Dice</button>';
-        document.getElementById('roll').addEventListener('click', function() {
-
-        console.log("dice rolled!");
-        throwDice();
-
+        document.getElementById('roll').addEventListener('click', function(){
+            throwDice();
         });
     }
 
-    // dice rolled is displayed and numbers are added to respective player's total score
-    function throwDice() {
-        rollSound.play();
+    function throwDice(){
         actionArea.innerHTML = '';
-        gameData.roll1 = Math.floor(Math.random() * 6) + 1; //using ceil could result in a zero
+        gameData.roll1 = Math.floor(Math.random() * 6) + 1;
         gameData.roll2 = Math.floor(Math.random() * 6) + 1;
-        game.innerHTML = `<p>Roll the dice for the ${gameData.players[gameData.index]}</p>`;
-        game.innerHTML += `<div id="diceRolled">
-                                <img id="dice" src="${gameData.dice[gameData.roll1-1]}">
-                                <img id="dice" src="${gameData.dice[gameData.roll2-1]}">
-                            </div>`;
-
+        game.innerHTML = `<p>Roll the dice for ${gameData.players[gameData.index]}</p>`;
+        game.innerHTML += `<img src="${gameData.dice[gameData.roll1-1]}"> <img src="${gameData.dice[gameData.roll2-1]}">`;
         gameData.rollSum = gameData.roll1 + gameData.roll2;
+        //console.log(gameData.rollSum);
 
-        // console.log(gameData.rollSum);
-
-        // if two 1's are rolled
-        if(gameData.rollSum === 2) {
-            // console.log("snake eyes were rolled");
-            game.innerHTML += '<p>Oh snap! Snake eyes!</p>';
+        if (gameData.rollSum === 2){
+            //console.log("snake eyes!");
+            game.innerHTML = '<p>Sorry, you have rolled snake eyes!</p>';
             gameData.score[gameData.index] = 0;
             gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-
-            // show the current score
-            setTimeout(setUpTurn, 2000);
-
             showCurrentScore();
-        }
-
-        // if either die is a 1
-        else if(gameData.roll1 === 1 || gameData.roll2 === 1) {
-            // console.log("one of the two dice was a 1");
-            gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-            game.innerHTML += `<p>Sorry, one of your rolls was a one, switching to ${    gameData.players[gameData.index]}</p>`;
-
             setTimeout(setUpTurn, 2000);
-        }
 
-        // if neither die is a 1
-        else {
-            // console.log("the game continues");
+        } else if (gameData.roll1 === 1 || gameData.roll2 === 1) {
+            //console.log("A one was rolled!");
+            gameData.index ? (gameData.index = 0) : (gameData.index = 1);
+            game.innerHTML += `<p>Sorry, one of your rolls was a 1, switching to ${gameData.players[gameData.index]}</p>`;
+            setTimeout(setUpTurn, 2000);
+        } else {
+            gulpSound.play();
+            //console.log("The game continues");
             gameData.score[gameData.index] = gameData.score[gameData.index] + gameData.rollSum;
-            actionArea.innerHTML = '<button id="rollagain">Roll again</button><button id="pass">Pass</button>';
-            
-            document.getElementById('rollagain').addEventListener('click', function() {
+            actionArea.innerHTML = '<button id="rollagain">Roll Again</button> or <button id="pass">Pass</button>';
+
+            document.getElementById('rollagain').addEventListener('click', function(){
                 setUpTurn();
             });
-
-            document.getElementById('pass').addEventListener('click', function() {
-                passSound.play();
+            document.getElementById('pass').addEventListener('click', function(){
                 gameData.index ? (gameData.index = 0) : (gameData.index = 1);
                 setUpTurn();
             });
-
+            //check winning condition
             checkWinningCondition();
         }
     }
 
-    // if player's total score is more than 29 points, they win the game
-    function checkWinningCondition() {
-        if(gameData.score[gameData.index] > gameData.gameEnd) {
-            game.innerHTML = `<h2 id="winner">${gameData.players[gameData.index]} wins with ${gameData.score[gameData.index]} points!</h2>`;
-
-            document.querySelector(".pig").setAttribute('src', 'images/winnerpig.png');
-
+    function checkWinningCondition(){
+        if (gameData.score[gameData.index] > gameData.gameEnd){
+            score.innerHTML = `<h2>${gameData.players[gameData.index]} wins the game with ${gameData.score[gameData.index]} points!</h2>`;
             actionArea.innerHTML = '';
-            document.getElementById('quit').innerHTML = "Start a New Game?";
+            document.getElementById('quit').innerHTML = "Start a new game?";
+        } else {
+            //show current score
+            showCurrentScore(); 
         }
-
-        else {
-            // show current score
-            score.innerHTML = `<p id="player1score"><strong>${gameData.players[0]}: ${gameData.score[0]}</strong></p> <p id="player2score"><strong>${gameData.players[1]}: ${gameData.score [1]}</strong></p>`;
-
-            showCurrentScore();
-        }
-
     }
 
-    // shows each player's total score
-    function showCurrentScore() {
-        score.innerHTML = `<p id="player1score">${gameData.players[0]}: <strong>${gameData.score[0]}</strong></p>
-                            <p id="player2score">${gameData.players[1]}: <strong>${gameData.score[1]}</strong></p>`;
+    function showCurrentScore(){
+        score.innerHTML = `<p><strong>Score for ${gameData.players[0]}: ${gameData.score[0]}</strong><br><strong>Score for ${gameData.players[1]}: ${gameData.score[1]}</strong>`;
     }
 
-}) ();
+  
+})();
